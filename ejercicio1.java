28c54d9d7971c7d79f2f71eb94370a5e3db6f9ed
Adrián Pobo <adrianpoboadell@gmail.com>
2021-07-06 12:05:21 +0200

    ejercicio1

---
+++ ejercicio1.java
@@ -0,0 +1,12 @@
+package Tarea02;
+
+public class ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println("FIN DEL PROGRAMA");
+	}
+
+}
